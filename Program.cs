546c72e3506d8e09af6f7da5fc6f211763ff5129
ElanurAltntp <62237450+ElanurAltntp@users.noreply.github.com>
2021-01-23 15:17:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace YazilimYetistirmeKursu_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //typesafety =tipgücenliği önemli C# ve Java da
+            string kategorietiketi = "kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizorani = 1.45;
+            bool sistemeGirisYapmisMi = true; //true
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if(dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if(dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi");
+            }
+
+
+            if(sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategorietiketi);
+        }
+    }
+}
